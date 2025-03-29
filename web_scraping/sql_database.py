6bb3f1f5183298c508e9bec8082e7d8acab2748e
ZuzanL <115154733+ZuzanL@users.noreply.github.com>
2025-03-29 15:17:19 +0100

    Update sql_database.py

--- web_scraping/sql_database.py
+++ web_scraping/sql_database.py
@@ -4,18 +4,18 @@ from sqlalchemy import create_engine
 
 df = pd.read_csv('real_estate_clean.csv')
 
-# Select columns to include (exclude the first three)
+# Selecting columns to include (exclude the first three)
 columns_to_include = df.columns[3:]
 df_to_sql = df[columns_to_include]
 
-engine = create_engine('sqlite:///real_estate.db')  # Creates a file named 'my_database.db'
+engine = create_engine('sqlite:///real_estate.db')
 
-# Write the DataFrame to the SQL database
+# Writing the DataFrame to the SQL database
 df_to_sql.to_sql('czech_RE', engine, if_exists='replace', index=False)
 
 connection = engine.raw_connection()
 
-read_df = pd.read_sql('SELECT * FROM czech_RE', connection)  # Pass the raw connection
+read_df = pd.read_sql('SELECT * FROM czech_RE', connection) 
 
 connection.close()
 

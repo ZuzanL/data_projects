bdfbcee4b6eee161e52359f4fddb75ea75050424
ZuzanL <115154733+ZuzanL@users.noreply.github.com>
2025-03-29 15:14:31 +0100

    Update sql_database.py

--- web_scraping/sql_database.py
+++ web_scraping/sql_database.py
@@ -8,19 +8,15 @@ df = pd.read_csv('real_estate_clean.csv')
 columns_to_include = df.columns[3:]
 df_to_sql = df[columns_to_include]
 
-# Create an SQLite engine (you can change this for other databases)
 engine = create_engine('sqlite:///real_estate.db')  # Creates a file named 'my_database.db'
 
 # Write the DataFrame to the SQL database
 df_to_sql.to_sql('czech_RE', engine, if_exists='replace', index=False)
 
-# Optional: You can read data from the database to check
-# Get the raw connection from the engine
 connection = engine.raw_connection()
 
 read_df = pd.read_sql('SELECT * FROM czech_RE', connection)  # Pass the raw connection
 
-# Close the raw connection when you're done
 connection.close()
 
 print(read_df)
